099b434e24fbf090439c27f80ff9d9148f7d4a56
santiagoPostacchini <postacchini.santiago.et21.21@gmail.com>
2024-11-21 21:30:11 -0300

    FireFly follow

---
+++ Assets/Scripts/Shaders/FireFlies.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlies : MonoBehaviour
+{
+    
+    public AudioSource audioSource;
+    [Header("HurtPP")]
+    [SerializeField] private Material m_HurtMaterial;
+    private float _hurtAmmount;
+    private string _intensityName = "_Intensity";
+
+
+}
